246bc4aeab510735b42df46db48de56a330f7bea
Maridel Macatula <maridelmacatula@lawn-143-215-93-18.lawn.gatech.edu>
2022-12-01 16:11:48 -0500

    Maridel

---
+++ myl-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h>Welcome to MyYearLive</h>
+
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
